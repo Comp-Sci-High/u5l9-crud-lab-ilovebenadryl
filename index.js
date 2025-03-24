498c390178789c5bc03f646b8aee6b06b08e5e88
ilovebenadryl <christian.ramirez25@compscihigh.org>
2025-03-24 19:12:21 +0000

    hi

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ const Country = mongoose.model("Country", countrySchema, "Countries");
 // Create a POST route for "/add/country" that adds a country using the request body (3 points)
 // Use postman to add at least THREE different countries
 
+app.post('/add/country', async (req, res)=> {
+  const me = await new country ({
+  country: "Honduras",
+  flagURL: 10954442,
+  rich: false,
+}).save()
+res.json(me)
+})
 
 // Create a GET route for "/" that renders countries.ejs with every country from the Countries collection (1 point)
 
